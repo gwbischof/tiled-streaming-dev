f81e65edefa3dab8ee46f20671cc5d9bddb26c8e
gbischof <bisc8233@gmail.com>
2024-11-26 15:36:57 -0500

    Clean up
    Co-authored-by: Hiran Wijesinghe <hyperrealist@users.noreply.github.com>

--- websocket_example.py
+++ websocket_example.py
@@ -24,55 +24,26 @@ app = FastAPI()
 data = []
 
 
-
-# @app.websocket("/ws")
-# async def websocket_endpoint(websocket: WebSocket):
-#     await websocket.accept()
-#     i = 0
-#     while True:
-#         data = {'data': i}
-#         output = json.dumps(data)
-#         await websocket.send_text(output)
-#         await asyncio.sleep(1)
-#         i += 1
-
-
-# @app.websocket("/ws_readwrite")
-# async def websocket_endpoint(websocket: WebSocket):
-#     await websocket.accept()
-#     i = 0
-#     while True:
-#         response = await websocket.receive_json()
-#         data = {'data': response, 'index': i}
-#         await websocket.send_json(data)
-#         await asyncio.sleep(1)
-#         i += 1
-
-
-# @app.websocket("/notify")
-# async def websocket_endpoint(websocket: WebSocket):
-#     await websocket.accept()
-#     i = 0
-#     while True:
-#         num = random.randint(0, 4)
-#         if num == 2:
-#             await websocket.send_json({"message": "new data", "index": i})
-#             await asyncio.sleep(1)
-#         i+=1
+@app.websocket("/notify")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    i = 0
+    while True:
+        num = random.randint(0, 4)
+        if num == 2:
+            await websocket.send_json({"message": "new data", "index": i})
+            await asyncio.sleep(1)
+        i += 1
 
 
 class Record(BaseModel):
     data: int
 
+
 @app.put("/insert")
 async def insert(record: Annotated[Record, Body(embed=True)]):
     data.append(record.data)
-    return {'uid': len(data)}
-
-# @app.put("/insert/{record}")
-# async def insert(record: int):
-#     data.append(record)
-#     return {'uid': len(data)}
+    return {"uid": len(data)}
 
 
 @app.websocket("/stream")
@@ -80,7 +51,7 @@ async def websocket_endpoint(websocket: WebSocket, cursor: int = 0):
     await websocket.accept()
     while True:
         if cursor < len(data):
-            await websocket.send_json({'record': data[cursor]})
+            await websocket.send_json({"record": data[cursor]})
             cursor += 1
         else:
             await asyncio.sleep(1)
@@ -89,7 +60,6 @@ async def websocket_endpoint(websocket: WebSocket, cursor: int = 0):
 @app.get("/")
 async def root():
     return {"message": "Testing"}
-
 
 
 @pytest.fixture(scope="session")
@@ -104,44 +74,34 @@ def api():
         yield client
 
 
-# def test_read(api_fixture):
-#     with api_fixture.websocket_connect("/ws") as websocket:
-#         for i in range(10):
-#             data = websocket.receive_json()
-#             print(data)
-
-
-# def test_readwrite(api_fixture):
-#     with api_fixture.websocket_connect("/ws_readwrite") as websocket:
-#         for i in range(10):
-#             message = {"message": i}
-#             websocket.send_json(message)
-#             data = websocket.receive_json()
-#             print(data)
-
 # def test_notify(api_fixture):
 #     with api_fixture.websocket_connect("/notify") as websocket:
 #         for i in range(10):
 #             data = websocket.receive_json()
 #             print(data)
 
+
 def test_insert(api_fixture):
     for i in range(5):
         response = api_fixture.put(
             "/insert",
-            json={'record': {'data': i}},
+            json={"record": {"data": i}},
         )
-        #assert response.status_code == 200
+        # assert response.status_code == 200
         print(response.json())
         print(data)
+
 
 def inserter():
     i = 0
     while True:
-        requests.put("http://127.0.0.1:8000/insert", data=json.dumps({'record': {'data': i}}))
+        requests.put(
+            "http://127.0.0.1:8000/insert", data=json.dumps({"record": {"data": i}})
+        )
         time.sleep(0.5)
-        i+=1
-    
+        i += 1
+
+
 @contextlib.contextmanager
 def inserter_process():
     try:
@@ -157,7 +117,7 @@ def inserter_process():
     finally:
         ps.terminate()
 
-    
+
 @contextlib.contextmanager
 def api_process():
     try:
@@ -173,7 +133,8 @@ def api_process():
     finally:
         ps.terminate()
 
-def test_asyncronous():
+
+def test_multiprocess():
     with api_process():
         with inserter_process():
             with connect("ws://localhost:8000/stream") as websocket:
@@ -186,34 +147,20 @@ def test_asyncronous():
                         data = websocket.receive_json()
                         print("websocket", data)
 
-def test_syncronous(api_fixture):
-    test_insert(api_fixture)
-    with api_fixture.websocket_connect("/stream?cursor=2") as websocket:
-        for i in range(3):
-            data = websocket.receive_json()
-            print("websocket", data)
-    # with api_fixture.websocket_connect("/stream", json={'cursor': 0}) as websocket:
-    #     for i in range(5):
-    #         response = api_fixture.put(
-    #             "/insert",
-    #             json={'record': {'data': i}},
-    #         )
-    #         print(response.json())
-    #         data = websocket.receive_json()
-    #         print(data)
 
-def inserter_thread():
-	for i in range(10):
-		data.append(i)
-		time.sleep(1)
+def test_threaded(api_fixture):
+    def inserter_thread():
+        for i in range(10):
+            data.append(i)
+            time.sleep(1)
 
-def test_async(api_fixture):
     t = threading.Thread(target=inserter_thread)
     t.start()
     with api_fixture.websocket_connect("/stream?cursor=2") as websocket:
         while True:
             data = websocket.receive_json()
             print("websocket", data)
+
 
 if __name__ == "__main__":
     uvicorn.run(app)
