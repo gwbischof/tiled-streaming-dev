7fc1df9df54e257fdda19aeb1adc145b61473700
Hiran Wijesinghe <wwijesing1@bnl.gov>
2024-12-06 16:14:20 -0500

    Working version without postgresql callbacks
    Co-authored-by: Garrett Bischof <gbischof@bnl.gov>

--- websocket_example.py
+++ websocket_example.py
@@ -1,18 +1,12 @@
-import contextlib
-import subprocess
-import sys
 from typing import Annotated
 from urllib import request
 import uvicorn
 import asyncio
 import json
 import pytest
-import random
 import time
-import requests
 import threading
 import psycopg2
-import uuid
 
 from fastapi import FastAPI, Body
 from fastapi import WebSocket
@@ -54,17 +48,16 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     cur.execute('SELECT * FROM datasets LIMIT 1;')
     uid, data, length = cur.fetchone()
-    old_cursor = cursor = length
+    old_length = length
     while True:
-        print("cursor", cursor)
-        print("data", data)
-        cur.execute('SELECT * FROM datasets LIMIT 1;')
-        uid, data, length = cur.fetchone()
-        cursor = length
-        if cursor > old_cursor:
-            await websocket.send_json({"message": "new data", "cursor": cursor})
-            old_cursor = cursor
-        await asyncio.sleep(1)
+        print(f"{length = }")
+        print(f"{data = }")
+        cur.execute('SELECT * FROM datasets WHERE uid=1 LIMIT 1;')
+        _, data, length = cur.fetchone()
+        if length > old_length:
+            await websocket.send_json({"message": "new data", "length": length})
+            old_length = length
+        await asyncio.sleep(0.5)
 
 
 class Record(BaseModel):
@@ -73,19 +66,33 @@ class Record(BaseModel):
 
 @app.put("/append")
 async def insert(record: Annotated[Record, Body(embed=True)]):
-    data.append(record.data)
-    return {"uid": len(data)}
+    cur.execute(
+        f"""
+            UPDATE datasets SET data = array_append(data, {record.data}) WHERE uid=1;
+            UPDATE datasets SET length = length + 1 WHERE uid=1;
+        """
+    )
+
+    cur.execute('SELECT length FROM datasets WHERE uid=1 LIMIT 1')
+
+    length = cur.fetchone()
+    return {"length": length}
 
 
 @app.websocket("/stream")
 async def websocket_endpoint(websocket: WebSocket, cursor: int = 0):
+    # How do you know when a dataset is completed?
+    # For this test we just send a None when the dataset is completed.
     await websocket.accept()
     while True:
-        if cursor < len(data):
+        cur.execute('SELECT * FROM datasets WHERE uid=1 LIMIT 1;')
+        _, data, length = cur.fetchone()
+        if cursor < length:
             await websocket.send_json({"record": data[cursor]})
             cursor += 1
         else:
             await asyncio.sleep(1)
+
 
 
 @app.get("/")
@@ -100,98 +107,16 @@ def api_fixture():
         yield client
 
 
-# @contextlib.contextmanager
-# def api():
-#     with TestClient(app) as client:
-#         yield client
-
-
-# def test_notify(api_fixture):
-#     with api_fixture.websocket_connect("/notify") as websocket:
-#         for i in range(10):
-#             data = websocket.receive_json()
-#             print(data)
-
-
-def test_insert(api_fixture):
-    for i in range(5):
-        response = api_fixture.put(
-            "/append",
-            json={"record": {"data": i}},
-        )
-        # assert response.status_code == 200
-        print(response.json())
-        print(data)
-
-
-def inserter():
-    i = 0
-    while True:
-        requests.put(
-            "http://127.0.0.1:8000/append", data=json.dumps({"record": {"data": i}})
-        )
-        time.sleep(0.5)
-        i += 1
-
-
-@contextlib.contextmanager
-def inserter_process():
-    try:
-        ps = subprocess.Popen(
-            [
-                sys.executable,
-                "-c",
-                f"from websocket_example import inserter; inserter()",
-            ]
-        )
-        time.sleep(0.5)
-        yield ps
-    finally:
-        ps.terminate()
-
-
-@contextlib.contextmanager
-def api_process():
-    try:
-        ps = subprocess.Popen(
-            [
-                sys.executable,
-                "-c",
-                f"from websocket_example import app; import uvicorn; uvicorn.run(app)",
-            ]
-        )
-        time.sleep(0.5)
-        yield ps
-    finally:
-        ps.terminate()
-
-
-def test_multiprocess():
-    with api_process():
-        with inserter_process():
-            with connect("ws://localhost:8000/stream") as websocket:
-                websocket.send("Hello world!")
-                message = websocket.recv()
-                print(message)
-            with api_fixture.websocket_connect("/stream") as websocket:
-                with inserter_process():
-                    while True:
-                        data = websocket.receive_json()
-                        print("websocket", data)
-
-
 def test_threaded(api_fixture):
-    def inserter_thread():
-        for i in range(10):
-            cur.execute(
-                f"""UPDATE datasets SET data = array_append(data, {i}) WHERE uid=1;
-                UPDATE datasets SET length = length + 1 WHERE uid=1;"""
-            )
 
-    t = threading.Thread(target=inserter_thread)
+    def inserter():
+        for i in range(8):
+            print("calling api_fixture.put")
+            api_fixture.put('/append', data=json.dumps({"record": {"data": i}}))
+            time.sleep(0.5)
+
+    t = threading.Thread(target=inserter)
     t.start()
-
-    cursor = 2
 
     print("Beginning")
     # Wait for a notification.
@@ -201,12 +126,12 @@ def test_threaded(api_fixture):
 
     print("Start stream")
     # Read the new data.
-    with api_fixture.websocket_connect(f"/stream?cursor={cursor}") as websocket:
-        while True:
+    with api_fixture.websocket_connect(f"/stream?cursor=2") as websocket:
+        for i in range(5):
             response = websocket.receive_json()
-            if response["record"] is None:
-                break
             print("websocket", response)
+    
+    t.join()
 
 
 def test_postgres_connectivity():
@@ -236,11 +161,6 @@ def test_postgres_connectivity():
     cur.execute('SELECT * FROM datasets LIMIT 1')
     print(cur.fetchone())
 
-
-# {'timestamp': "time", 'uid': 123123, length: 2 'data': [1, 2]}
-# TODO:
-# - update the inserter to append to the data list in datasets table
-# - update notify and stream endpoints to poll from postgress and stream
 
 if __name__ == "__main__":
     uvicorn.run(app)
