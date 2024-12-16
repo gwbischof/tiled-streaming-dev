7e36b8ee77b160febb2f20f9c9aac7500d4e4322
gbischof <bisc8233@gmail.com>
2024-12-16 17:46:39 -0500

    use asyncpg

--- websocket_example.py
+++ websocket_example.py
@@ -6,68 +6,70 @@ import pytest
 import time
 import threading
 import psycopg2
+import asyncpg
 
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Depends
 from fastapi import WebSocket
 from fastapi.testclient import TestClient
 from pydantic import BaseModel
+from httpx import ASGITransport, AsyncClient
 
 app = FastAPI()
+app.pool = None
 
-conn = psycopg2.connect(
-    dbname="streaming-test-postgres",
-    user="postgres",
-    host="localhost",
-    password="secret",
-)
+async def db_init():
+    app.pool = await asyncpg.create_pool(user='postgres', password='secret',
+                                  database='streaming-test-postgres', host='localhost')
 
-# Autocommit after each command.
-conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    # Take a connection from the pool.
+    async with app.pool.acquire() as connection:
+        # Open a transaction.
+        async with connection.transaction():
 
+            await connection.execute("DROP TABLE IF EXISTS datasets;")
 
-def db_init():
-    cur = conn.cursor()
+            await connection.execute(
+                """
+                CREATE TABLE datasets (
+                    uid integer,
+                    data integer[],
+                    length integer
+                );
+            """
+            )
 
-    cur.execute("DROP TABLE IF EXISTS datasets;")
+            await connection.execute(
+                f"""
+                INSERT INTO datasets (uid, data, length)
+                    VALUES (1, '{{1, 2, 3}}', 3);
+            """
+            )
 
-    cur.execute(
-        """
-        CREATE TABLE datasets (
-            uid integer,
-            data integer[],
-            length integer
-        );
-    """
-    )
-
-    cur.execute(
-        f"""
-        INSERT INTO datasets (uid, data, length)
-            VALUES (1, '{{1, 2, 3}}', 3);
-    """
-    )
-
-    return cur
+    print("done")
 
 
 @app.websocket("/notify")
 async def websocket_endpoint(websocket: WebSocket):
-    cur = conn.cursor()
     await websocket.accept()
 
-    # Listen for notifications on channel `notrigger_test`.
-    cur.execute("LISTEN notrigger_test;")
+    # Take a connection from the pool.
+    async with app.pool.acquire() as connection:
+        # Open a transaction.
+        async with connection.transaction():
+            # Listen for notifications on channel `notrigger_test`.
+            await connection.execute("LISTEN notrigger_test;")
 
-    while True:
-        # TODO: Need to figure out how to make poll not block indefinitely.
-        await asyncio.sleep(0.5)
-        conn.poll()
-        for notify in conn.notifies:
-            await websocket.send_json({"notification": notify.payload})
-            print(notify.payload)
-        conn.notifies.clear()
+            async def callback(conn, pid, channel, payload):
+                await websocket.send_json({"notification": payload})
+                print(payload)
+
+            await connection.add_listener('notrigger_test', callback)
+            await connection.execute('LISTEN notrigger_test')
+
+            while True:
+                await connection.wait()  # wait for notification
 
 
 class Record(BaseModel):
@@ -76,32 +78,34 @@ class Record(BaseModel):
 
 @app.put("/append")
 async def append(record: Annotated[Record, Body(embed=True)]):
-    cur = conn.cursor()
-    cur.execute(
-        f"""
-            UPDATE datasets SET data = array_append(data, {record.data}) WHERE uid=1;
-            UPDATE datasets SET length = length + 1 WHERE uid=1;
-        """
-    )
 
-    # Create a notification on channel `notrigger_test` so that can be picked
-    # up by listeners of this channel.
-    cur.execute(f"NOTIFY notrigger_test, 'added data: {record.data}';")
+    # Take a connection from the pool.
+    async with app.pool.acquire() as connection:
+        # Open a transaction.
+        async with connection.transaction():
+    
+            await connection.execute(
+                f"""
+                    UPDATE datasets SET data = array_append(data, {record.data}) WHERE uid=1;
+                    UPDATE datasets SET length = length + 1 WHERE uid=1;
+                """
+            )
 
-    cur.execute("SELECT length FROM datasets WHERE uid=1 LIMIT 1;")
-    length = cur.fetchone()
+            # Create a notification on channel `notrigger_test` so that can be picked
+            # up by listeners of this channel.
+            await connection.execute(f"NOTIFY notrigger_test, 'added data: {record.data}';")
 
-    print(f"appended {record = }")
-    return {"length": length}
+            val = await connection.fetchval("SELECT length FROM datasets WHERE uid=1 LIMIT 1;")
+            print(f"appended {record = }")
+            return {"length": val}
 
 
 @app.websocket("/stream")
 async def websocket_endpoint(websocket: WebSocket, cursor: int = 0):
     # How do you know when a dataset is completed?
-    cur = conn.cursor()
     await websocket.accept()
     while True:
-        cur.execute("SELECT * FROM datasets WHERE uid=1 LIMIT 1;")
+        await app.conn.execute("SELECT * FROM datasets WHERE uid=1 LIMIT 1;")
         _, data, length = cur.fetchone()
         if cursor < length:
             await websocket.send_json({"record": data[cursor]})
@@ -115,38 +119,51 @@ async def root():
     return {"message": "Testing"}
 
 
-@pytest.fixture(scope="session")
-def api_fixture():
-    with TestClient(app) as client:
-        db_init()
-        yield client
+# @pytest.fixture(scope="session")
+# async def api_fixture():
+#     async with TestClient(app) as client:
+#         await db_init()
+#         yield client
+#client = TestClient(app)
+#asyncio.run(db_init())
 
 
-def test_threaded(api_fixture):
+@pytest.mark.anyio
+async def test_threaded():
 
     def inserter():
         for i in range(8):
             print("calling api_fixture.put")
-            api_fixture.put("/append", data=json.dumps({"record": {"data": i}}))
+            #client.put("/append", data=json.dumps({"record": {"data": i}}))
             time.sleep(0.5)
 
     t = threading.Thread(target=inserter)
     t.start()
 
-    # Wait for a notification.
-    with api_fixture.websocket_connect("/notify") as notify_websocket:
-        notification = notify_websocket.receive_json()
-        print("notification", notification)
+    await db_init()
 
-    print("Start stream")
-    # Read the new data.
-    with api_fixture.websocket_connect(f"/stream?cursor=2") as websocket:
-        for i in range(5):
-            response = websocket.receive_json()
-            print("websocket", response)
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://localhost"
+    ) as ac:
+        response = await ac.get("/")
+
+    # Wait for a notification.
+    # with client.websocket_connect("/notify") as notify_websocket:
+    #     notification = notify_websocket.receive_json()
+    #     print("notification", notification)
+
+    # print("Start stream")
+    # # Read the new data.
+    # with api_fixture.websocket_connect(f"/stream?cursor=2") as websocket:
+    #     for i in range(5):
+    #         response = websocket.receive_json()
+    #         print("websocket", response)
 
     t.join()
 
 
 if __name__ == "__main__":
+
+    #asyncio.run(db_init())
+
     uvicorn.run(app)
