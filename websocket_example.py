5ab708c0501c7c71207b6f8bcddf5ae60216828d
gbischof <bisc8233@gmail.com>
2025-01-03 13:04:58 -0500

    replace uid with path

--- websocket_example.py
+++ websocket_example.py
@@ -60,14 +60,14 @@ async def db_init():
     print("done")
 
 
-@app.websocket("/notify/{uid:path}")
-async def notify(uid: str, websocket: WebSocket):
+@app.websocket("/notify/{path:path}")
+async def notify(path: str, websocket: WebSocket):
     """
     Websocket endpoint to receive notifications about new data.
 
     Parameters
     ----------
-    uid : str
+    path : str
         The uid of the dataset to receive notifications about.
     websocket : WebSocket
         The websocket connection.
@@ -78,14 +78,16 @@ async def notify(uid: str, websocket: WebSocket):
     """
 
     await websocket.accept()
-    uid = 1
+
     # Take a connection from the pool.
     async with app.pool.acquire() as connection:
 
         async def callback(conn, pid, channel, payload):
             await websocket.send_json({})
 
-        await connection.add_listener(f"notifications_{uid}", callback)
+        await connection.add_listener(
+            f"notifications_{path.replace('/', "_")}", callback
+        )
 
         while True:
             await asyncio.sleep(1)
@@ -95,14 +97,14 @@ class Record(BaseModel):
     data: int
 
 
-@app.put("/append/{uid}")
-async def append(uid: str, record: Annotated[Record, Body(embed=True)]):
+@app.put("/append/{path:path}")
+async def append(path: str, record: Annotated[Record, Body(embed=True)]):
     """
     Add a new item to the dataset and notify listeners.
 
     Parameters
     ----------
-    uid : str
+    path : str
         The unique identifier of the dataset to which the new item will be appended.
     record : Annotated[Record, Body(embed=True)]
         The record containing the data to be appended to the dataset.
@@ -115,6 +117,7 @@ async def append(uid: str, record: Annotated[Record, Body(embed=True)]):
     async with app.pool.acquire() as connection:
         async with connection.transaction():
             # Append new value to data and increment the length.
+            uid = path.split('/')[-1]
             await connection.execute(
                 f"""
                     UPDATE datasets SET data = array_append(data, {record.data}) WHERE uid={uid};
@@ -122,10 +125,10 @@ async def append(uid: str, record: Annotated[Record, Body(embed=True)]):
                 """
             )
 
-            # Create a notification on channel `notifications_{uid}` so that it can be picked
+            # Create a notification on channel `notifications_{path}` so that it can be picked
             # up by listeners of this channel.
             await connection.execute(
-                f"NOTIFY notifications_{uid}, 'added data: {record.data}';"
+                f"NOTIFY notifications_{path.replace('/', "_")}, 'added data: {record.data}';"
             )
             # Create a notification on channel `notifications_all` so that generic listeners
             # can be notified of update.
@@ -134,13 +137,13 @@ async def append(uid: str, record: Annotated[Record, Body(embed=True)]):
             )
 
 
-@app.websocket("/stream/{uid}")
-async def websocket_endpoint(uid: str, websocket: WebSocket, cursor: int = 0):
+@app.websocket("/stream/{path:path}")
+async def websocket_endpoint(path: str, websocket: WebSocket, cursor: int = 0):
     """
     WebSocket endpoint to stream dataset records to the client.
     Parameters
     ----------
-    uid : str
+    path : str
         Unique identifier for the dataset.
     websocket : WebSocket
         WebSocket connection instance.
@@ -153,12 +156,13 @@ async def websocket_endpoint(uid: str, websocket: WebSocket, cursor: int = 0):
 
     # How do you know when a dataset is completed?
     await websocket.accept()
+    uid = path.split("/")[-1]
     while True:
         async with app.pool.acquire() as connection:
             uid, data, length = await connection.fetchrow(
                 f"SELECT * FROM datasets WHERE uid={uid} LIMIT 1;"
             )
-            print(f"server {uid = }, {data = }")
+            print(f"server {path = }, {data = }")
         if cursor < length:
             await websocket.send_json({"record": data[cursor]})
             cursor += 1
@@ -184,42 +188,42 @@ async def test_async():
 
     await db_init()
 
-    async def inserter(uid):
+    async def inserter(path):
         nonlocal ac
         await asyncio.sleep(2.0)
         for i in range(4):
             await ac.put(
-                f"http://localhost/append/{uid}",
+                f"http://localhost/append/{path}",
                 content=json.dumps({"record": {"data": i}}),
             )
-            print(f"client appended {uid = }, record {i}")
+            print(f"client appended {path = }, record {i}")
             await asyncio.sleep(1)
 
-    async def notification_listener(uid):
+    async def notification_listener(path):
         nonlocal ac
-        async with aconnect_ws(f"http://localhost/notify/{uid}", ac) as ws:
+        async with aconnect_ws(f"http://localhost/notify/{path}", ac) as ws:
             for i in range(3):
                 message = await ws.receive_json()
-                print(f"client received notification {uid = }, {message = }")
+                print(f"client received notification {path = }, {message = }")
                 await asyncio.sleep(1)
 
-    async def stream_listener(uid):
+    async def stream_listener(path):
         nonlocal ac
-        async with aconnect_ws(f"http://localhost/stream/{uid}?cursor=1", ac) as ws:
+        async with aconnect_ws(f"http://localhost/stream/{path}?cursor=1", ac) as ws:
             for i in range(3):
                 message = await ws.receive_json()
-                print(f"client received data {uid = }, {message=}")
+                print(f"client received data {path = }, {message=}")
                 await asyncio.sleep(1)
 
     ac = AsyncClient(
         transport=ASGIWebSocketTransport(app=app), base_url="http://localhost"
     )
     async with asyncio.TaskGroup() as tg:
-        tg.create_task(inserter(1))
-        tg.create_task(inserter(2))
-        tg.create_task(notification_listener(1))
+        tg.create_task(inserter("1"))
+        tg.create_task(inserter("2"))
+        tg.create_task(notification_listener("1"))
         tg.create_task(notification_listener("all"))
-        tg.create_task(stream_listener(2))
+        tg.create_task(stream_listener("2"))
 
 
 if __name__ == "__main__":
