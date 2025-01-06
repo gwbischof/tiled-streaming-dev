50e7c32aae20519c1ee668525f1d4adf2734b71b
Hiran Wijesinghe <wwijesing1@bnl.gov>
2025-01-06 16:41:23 -0500

    working prototype
    Co-authored-by: Garrett Bischof <gbischof@bnl.gov>

--- websocket_example.py
+++ websocket_example.py
@@ -39,27 +39,13 @@ async def db_init():
                 """
                 CREATE TABLE datasets (
                     path text,
-                    uid integer,
+                    uid text,
                     data integer[],
-                    length integer
+                    length integer,
+                    UNIQUE (path, uid)
                 );
-            """
+                """
             )
-
-            await connection.execute(
-                f"""
-                INSERT INTO datasets (path, uid, data, length)
-                    VALUES ('root', 1, '{{1, 2, 3}}', 3);
-            """
-            )
-            await connection.execute(
-                f"""
-                INSERT INTO datasets (path, uid, data, length)
-                    VALUES ('root', 2, '{{100, 101, 102}}', 3);
-            """
-            )
-
-    print("done")
 
 
 def path_hash(path: str) -> str:
@@ -69,7 +55,7 @@ def path_hash(path: str) -> str:
     # ensure hash is a valid postgres identifier by prefixing with a string
     # valid identifiers start with a letter or "_" and contain only letters, numbers, and "_"
     # and are at most 63 characters long
-    return "shake256" + hashlib.shake_256(path.encode()).hexdigest(20)
+    return "shake256_" + hashlib.shake_256(path.encode()).hexdigest(20)
 
 
 @app.websocket("/notify/{path:path}")
@@ -80,13 +66,9 @@ async def notify(path: str, websocket: WebSocket):
     Parameters
     ----------
     path : str
-        The uid of the dataset to receive notifications about.
+        The catalog path.
     websocket : WebSocket
         The websocket connection.
-
-    Returns
-    -------
-    None
     """
 
     await websocket.accept()
@@ -97,9 +79,7 @@ async def notify(path: str, websocket: WebSocket):
         async def callback(conn, pid, channel, payload):
             await websocket.send_json({})
 
-        await connection.add_listener(
-            f"n_{path_hash(path)}", callback
-        )
+        await connection.add_listener(f"{path_hash(path)}", callback)
 
         while True:
             await asyncio.sleep(1)
@@ -110,70 +90,73 @@ class Record(BaseModel):
 
 
 @app.put("/append/{path:path}/{uid}")
-async def append(path: str, uid: int, record: Annotated[Record, Body(embed=True)]):
+async def append(path: str, uid: str, record: Annotated[Record, Body(embed=True)]):
     """
     Add a new item to the dataset and notify listeners.
 
     Parameters
     ----------
+    uid: str
+        The unique indentifier for the dataset.
     path : str
-        The unique identifier of the dataset to which the new item will be appended.
+        The catalog path.
     record : Annotated[Record, Body(embed=True)]
         The record containing the data to be appended to the dataset.
-
-    Returns
-    -------
-    None
     """
 
     async with app.pool.acquire() as connection:
         async with connection.transaction():
-            # Append new value to data and increment the length.
+            # Append new value to data or create new record and increment the length.
             await connection.execute(
                 f"""
-                    UPDATE datasets SET data = array_append(data, {record.data}) WHERE uid={uid} AND path='{path}';
-                    UPDATE datasets SET length = length + 1 WHERE uid={uid} AND path='{path}';
+                    INSERT INTO datasets (path, uid, data, length)
+                    VALUES ('{path}', '{uid}', '{{{record.data}}}', 1)
+                    ON CONFLICT (path, uid) DO UPDATE
+                    SET data = array_append(datasets.data, {record.data}),
+                        length = datasets.length + 1;
                 """
             )
 
-            # Create a notification on the dataset and parent channels.            
-            split_path = path.split('/') + [str(uid)]
-            for i in range(1, len(split_path) +1):
-                sub_path = '/'.join(split_path[0:i])
+            # Create a notification on the dataset and parent channels.
+            split_path = path.split("/") + [str(uid)]
+            for i in range(1, len(split_path) + 1):
+                sub_path = "/".join(split_path[0:i])
                 await connection.execute(
-                    f"NOTIFY n_{path_hash(sub_path)}, 'added data: {record.data}';"
+                    f"NOTIFY {path_hash(sub_path)}, 'added data: {record.data}';"
                 )
 
 
 @app.websocket("/stream/{path:path}/{uid}")
-async def websocket_endpoint(path: str, uid: str,  websocket: WebSocket, cursor: int = 0):
+async def websocket_endpoint(
+    path: str, uid: str, websocket: WebSocket, cursor: int = 0
+):
     """
     WebSocket endpoint to stream dataset records to the client.
+
     Parameters
     ----------
+    uid : str
+        unique indentifier for the dataset.
     path : str
-        Unique identifier for the dataset.
+        catalog path.
     websocket : WebSocket
         WebSocket connection instance.
     cursor : int, optional
         Starting position in the dataset (default is 0).
-    Returns
-    -------
-    None
     """
-
     # How do you know when a dataset is completed?
     await websocket.accept()
     while True:
         async with app.pool.acquire() as connection:
-            path, uid, data, length = await connection.fetchrow(
-                f"SELECT * FROM datasets WHERE uid={uid} AND path='{path}' LIMIT 1;"
+            result = await connection.fetchrow(
+                f"SELECT * FROM datasets WHERE uid='{uid}' AND path='{path}' LIMIT 1;"
             )
-            print(f"server {path = }, {data = }")
-        if cursor < length:
-            await websocket.send_json({"record": data[cursor]})
-            cursor += 1
-        else:
+            if result is not None:
+                path, uid, data, length = result
+                print(f"server {path = }, {data = }")
+                while cursor < length:
+                    await websocket.send_json({"record": data[cursor]})
+                    cursor += 1
             await asyncio.sleep(1)
 
 
@@ -181,16 +164,6 @@ async def websocket_endpoint(path: str, uid: str,  websocket: WebSocket, cursor:
 async def test_async():
     """
     Asynchronous test function to test all of the components of streaming data together.
-
-    FastAPI endpoints:
-    /notify/{uid} - Websocket endpoint to receive notifications about new data.
-    /notify/all - Websocket endpoint to receive notifications about new data for all datasets.
-    /stream/{uid} - Websocket endpoint to stream dataset records to the client.
-
-    This test initializes a database connection and creates multiple asynchronous tasks to:
-    1. Insert data into the server.
-    2. Listen for notifications from the server.
-    3. Listen for data streams from the server.
     """
 
     await db_init()
@@ -226,12 +199,18 @@ async def test_async():
         transport=ASGIWebSocketTransport(app=app), base_url="http://localhost"
     )
     async with asyncio.TaskGroup() as tg:
-        tg.create_task(inserter("root/1"))  # Insert into dataset 1.
-        tg.create_task(inserter("root/2"))  # Insert into dataset 2.
-        tg.create_task(notification_listener("root/1"))  # Get notifications for dataset 1.
-        tg.create_task(notification_listener("root"))  # Get notifications for the parent of dataset 1.
-        tg.create_task(stream_listener("root/1"))  # Get dataset 1 data stream.
-        tg.create_task(stream_listener("root/2"))  # Get dataset 2 data stream.
+        # Insert into dataset 1.
+        tg.create_task(inserter("root/1"))
+        # Insert into dataset 2.
+        tg.create_task(inserter("root/2"))
+        # Get notifications for dataset 1.
+        tg.create_task(notification_listener("root/1"))
+        # Get notifications for the parent of dataset 1.
+        tg.create_task(notification_listener("root"))
+        # Get dataset 1 data stream.
+        tg.create_task(stream_listener("root/1"))
+        # Get dataset 2 data stream.
+        tg.create_task(stream_listener("root/2"))
 
 
 if __name__ == "__main__":
